f97996e6b6baa295c638a0443b61919e4e05e928
fox <>
2019-10-18 10:42:07 -0500

    Initial upload

---
+++ Cron.py
@@ -0,0 +1,44 @@
+from time import sleep
+import datetime
+import Settings
+import json
+import subprocess
+from SQSConnection import SQSConnection
+from threading import Thread
+
+
+def execute_test(script):
+    print('Se ejecuta la prueba')
+    txt = script
+
+    with open(Settings.CYPRESS_PATH + "/cypress/integration/test.js", "w+") as file:
+        file.write(txt)
+
+    output = subprocess.call([Settings.ANDROID_HOME+'/platform-tools/adb',txt])
+    if output < 0:
+        print('error en ejecución de prueba')
+
+def process():
+    try:
+        sqs_connection = SQSConnection(Settings.AWS_QUEUE_URL_OUT_CYPRESS)
+
+        with sqs_connection:
+            sqs_connection.receive()
+            if sqs_connection.message is not '':
+                message_body = sqs_connection.message.get('Body')
+                msg = json.loads(message_body)
+                #Aqui va la conversion del json
+                script = msg['script']
+                sqs_connection.delete()
+                execute_test(script)
+
+    except Exception as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    while True:
+        Thread(target=process).start()
+        st = str(datetime.datetime.now())
+        print(st + ' : alive')
+        sleep(Settings.SLEEP_TIME)
---
+++ Settings.py
@@ -0,0 +1,10 @@
+import os
+
+MEDIA_DIR = '/home/ec2-user/apps/Cron/'
+ANDROID_HOME = os.environ.get('ANDROID_HOME')
+AWS_ACCESS_KEY_ID_SQS = os.environ.get('AWS_ACCESS_KEY_ID_SQS')
+AWS_SECRET_ACCESS_KEY_SQS = os.environ.get('AWS_SECRET_ACCESS_KEY_SQS')
+AWS_REGION_SQS = os.environ.get('AWS_REGION_SQS')
+AWS_QUEUE_URL_IN = os.environ.get('AWS_QUEUE_URL_IN')
+AWS_QUEUE_URL_OUT_CYPRESS = os.environ.get('AWS_QUEUE_URL_OUT_CYPRESS')
+SLEEP_TIME = 5
